4f21e7d47713ce874dd2acb14d8bc68b510571b0
colecody27 <colecody27@gmail.com>
2024-01-29 11:43:38 -0800

    Add bruteforce login method

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { onMount } from 'svelte';
+	import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+	import auth from '../lib/firebase/firebase.client'
+
 </script>
 
 <!-- App Shell -->
@@ -9,32 +13,16 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-4xl uppercase">Candor</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a
-					class="btn btn-sm variant-ghost-surface"
+					class="btn btn variant-ghost-surface"
 					href="https://discord.gg/EXqV7W8MtY"
 					target="_blank"
 					rel="noreferrer"
 				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
+					Login | Sign Up
 				</a>
 			</svelte:fragment>
 		</AppBar>
